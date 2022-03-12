6cc1fb0fa9dd031e72e7768c3c0cfaa4a7c417eb
vishal <vrana14693@gmail.com>
2022-03-12 12:50:47 +0530

    api key for recipes generated

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+require("dotenv").config();
 
 function Popular() {
-  return <div>Popular</div>;
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_RECIPES_API}&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Popular;
