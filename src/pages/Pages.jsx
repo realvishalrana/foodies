fb49d49443c94fb05743189df2381beef72dc4c9
vishal <vrana14693@gmail.com>
2022-03-13 20:39:08 +0530

    category components & searched page done

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Home from "./Home";
-
+import { Route, Routes } from "react-router-dom";
+import Cuisine from "./Cuisine";
+import Searched from "./Searched";
 function Pages() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
+    </Routes>
   );
 }
 
